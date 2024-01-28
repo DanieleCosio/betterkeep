c50a20808860f58ccbc367f3d7819e613f91434f
daniele.cosio <daniele.cosio99@gmail.com>
2024-01-28 16:25:16 +0100

    Store refactor

--- src/stores/Notes.ts
+++
@@ -1,6 +0,0 @@
-import type { NoteProps } from '$types/Note';
-import { writable, type Writable } from 'svelte/store';
-
-const notesStore: Writable<NoteProps[]> = writable([]);
-
-export default notesStore;
